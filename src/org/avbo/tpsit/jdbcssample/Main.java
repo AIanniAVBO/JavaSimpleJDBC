114a92d1c8038401ca3a7ad666e3cfd9c772cfa3
Andrea Iannì <andrea.ianni@avbo.it>
2024-04-17 16:32:20 +0200

    Effettuata la connessione al database all'interno della classe Main

--- src/org/avbo/tpsit/jdbcssample/Main.java
+++ src/org/avbo/tpsit/jdbcssample/Main.java
@@ -1,8 +1,47 @@
 package org.avbo.tpsit.jdbcssample;
 
-public class Main {
+import java.sql.*;
 
+public class Main {
+	private static String DB_Path = "example.db";
+	/**
+	 * Metodo che fornisce la stringa di connessione
+	 * al database
+	 * @return Stringa di connessione da utilizzare 
+	 * per connettersi al database
+	 */
+	private static String GetConnectionString()
+	{
+		return "jdbc:sqlite:" + DB_Path;
+	}
+	
+	private static String GetUsername()
+	{
+		return null;
+	}
+	
+	private static String GetPassword()
+	{
+		return null;
+	}
+	
+	
 	public static void main(String[] args) {
+		//Apre la connessione con il database
+		try (Connection conn = 
+				DriverManager.getConnection(
+						//Utilizzando la stringa di connessione
+						GetConnectionString(), 
+						//E nome utente e password se presenti
+						GetUsername(), GetPassword());)
+		{
+			
+		} catch (SQLException e) {
+			//Se arriva qui significa che non è
+			//	riuscito a connettersi al database
+			System.out.println("Errore durante la connessione al database");
+			e.printStackTrace();
+		}
 		
 	}
 
